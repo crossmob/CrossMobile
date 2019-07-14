7e5d95504b02fed647f14453b117579efa4b85a8
Panayotis Katsaloulis <panayotis@panayotis.com>
2019-07-14 18:11:15 +0300

    better partitioning of billing package

---
+++ cmioslayer/src/main/java/org/crossmobile/backend/android/billing/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * (c) 2019 by Panayotis Katsaloulis
+ *
+ * CrossMobile is free software; you can redistribute it and/or modify
+ * it under the terms of the CrossMobile Community License as published
+ * by the CrossMobile team.
+ *
+ * CrossMobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * CrossMobile Community License for more details.
+ *
+ * You should have received a copy of the CrossMobile Community
+ * License along with CrossMobile; if not, please contact the
+ * CrossMobile team at https://crossmobile.tech/contact/
+ */
+@CMLib(name = "cmpayment")
+package org.crossmobile.backend.android.billing;
+
+import org.crossmobile.bridge.ann.CMLib;
