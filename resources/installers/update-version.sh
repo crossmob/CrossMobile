dcd6a0e8b75e4f24586e75c52a960bf260674695
Panayotis Katsaloulis <panayotis@panayotis.com>
2019-08-27 17:48:01 +0300

    update-version script handles missing archetypes

--- resources/installers/update-version.sh
+++ resources/installers/update-version.sh
@@ -21,11 +21,15 @@ mvn versions:set -DnewVersion="$VERSION" -DgenerateBackupPoms=false
 
 xmlstarlet ed -P -L -u "/_:project/_:properties/_:long.version" -v "$LONGVERSION" pom.xml
 xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmutils/cmutils-tools/src/main/resources/templates/pom_xml
+xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-button/src/main/resources/archetype-resources/pom.xml
+xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-camera/src/main/resources/archetype-resources/pom.xml
 xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-empty/src/main/resources/archetype-resources/pom.xml
+xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-i18n/src/main/resources/archetype-resources/pom.xml
+xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-map/src/main/resources/archetype-resources/pom.xml
 xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-navigation/src/main/resources/archetype-resources/pom.xml
-xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-sample/src/main/resources/archetype-resources/pom.xml
 xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-single/src/main/resources/archetype-resources/pom.xml
 xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-storyboard/src/main/resources/archetype-resources/pom.xml
+xmlstarlet ed -P -L -u "/_:project/_:parent/_:version" -v "$VERSION" cmarchetypes/cmarchetype-table/src/main/resources/archetype-resources/pom.xml
 xmlstarlet ed -P -L -u "/_:project/_:properties/_:crossmobile.version" -v "$VERSION" cmprojects/cmproject/pom.xml
 xmlstarlet ed -P -L -u  '/repositories/repository[@id="themes"]/plugins/plugin/version' -v "$VERSION" cmutils/cmutils-tools/src/main/resources/plugins/baseplugins.xml
 xmlstarlet ed -P -L -u  '/repositories/repository[@id="crossmobile"]/plugins/plugin/version' -v "$VERSION" cmutils/cmutils-tools/src/main/resources/plugins/baseplugins.xml
