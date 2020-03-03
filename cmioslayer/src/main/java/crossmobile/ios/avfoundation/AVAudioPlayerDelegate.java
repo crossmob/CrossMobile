b045545e5293be3e9a9394d60057a1faf07f5285
Panayotis Katsaloulis <panayotis@panayotis.com>
2020-03-04 01:24:13 +0200

    add SPDX headers

--- cmioslayer/src/main/java/crossmobile/ios/avfoundation/AVAudioPlayerDelegate.java
+++ cmioslayer/src/main/java/crossmobile/ios/avfoundation/AVAudioPlayerDelegate.java
@@ -1,19 +1,6 @@
-/*
- * (c) 2020 by Panayotis Katsaloulis
- *
- * CrossMobile is free software; you can redistribute it and/or modify
- * it under the terms of the CrossMobile Community License as published
- * by the CrossMobile team.
- *
- * CrossMobile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * CrossMobile Community License for more details.
- *
- * You should have received a copy of the CrossMobile Community
- * License along with CrossMobile; if not, please contact the
- * CrossMobile team at https://crossmobile.tech/contact/
- */
+// (c) 2020 by Panayotis Katsaloulis
+// SPDX-License-Identifier: LGPL-3.0-only
+
 package crossmobile.ios.avfoundation;
 
 import crossmobile.ios.foundation.NSError;
--- resources/installers/check-headers.sh
+++ resources/installers/check-headers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in `find . -name *.java | grep -v './cmioslayer/src/main/java/org/crossmobile/support/cassowary' | grep -v '/__artifactId__/' | grep -v '^./cmioslayer/src/main/java/org/crossmobile/backend/android/billing' | grep -v '^./cmutils/cmutils-common/src/main/java/org/robovm/objc/block' | grep -v '^./cmbuild/cmbuild-plugins/src/main/java/org/crossmobile/plugin/parser/antlr' | grep -v './cmutils/cmutils-tools/src/main/java/org/crossmobile/utils/StringEscapeUtilsTrimmed.java' | grep -v '^./cmbuild/cmbuild-maven-plugin/src/main/java/org/crossmobile/build/antlr' | grep -v '^./cmbuild/cmbuild-android-extra/src/main/java/org/crossmobile/backend/android' | grep -v '^./cmioslayer/src/main/java/org/crossmobile/backend/android/web/VideoEnabledWeb' | grep -v '^./cmioslayer/src/main/java/org/crossmobile/support/MiGBase64.java' | grep -v '^./cmioslayer/src/main/java/com/android/vending/billing/IInAppBillingService.java' | grep -v '/target/generated-sources/'` ; do
-    q=`grep 'it under the terms of the CrossMobile Community License as published' $i`
+    q=`grep 'SPDX-License-Identifier' $i`
     if [ "$q" == "" ] ; then
         echo $i
     fi
