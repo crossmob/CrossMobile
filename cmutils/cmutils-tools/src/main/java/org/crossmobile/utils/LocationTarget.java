2f9f7e9fc7204f0fcba48b4452ed864154bd44d2
Panayotis Katsaloulis <panayotis@panayotis.com>
2020-06-10 17:16:17 +0300

    Force JDK 1.8 only

--- cmutils/cmutils-tools/src/main/java/org/crossmobile/utils/LocationTarget.java
+++ cmutils/cmutils-tools/src/main/java/org/crossmobile/utils/LocationTarget.java
@@ -12,27 +12,32 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class LocationTarget {
 
     private final String appName;
     private final Collection<LocationEntry> paths = new ArrayList<>();
 
-    public LocationTarget(Execs exec) {
-        this(exec, (String[]) null);
-    }
-
     public LocationTarget(String... morePaths) {
-        this(null, morePaths);
+        this(null, null, morePaths);
     }
 
     public LocationTarget(Execs exec, String... morePaths) {
+        this(exec, null, morePaths);
+    }
+
+    public LocationTarget(Predicate<File> predicate, String... morePaths) {
+        this(null, predicate, morePaths);
+    }
+
+    private LocationTarget(Execs exec, Predicate<File> predicate, String... morePaths) {
         this.appName = exec == null ? null : exec.name().toLowerCase();
         if (appName != null)
-            paths.add(new LocationEntry(appName, false));
+            paths.add(new LocationEntry(appName, false, predicate));
         if (morePaths != null && morePaths.length > 0)
             for (String path : morePaths)
-                paths.add(new LocationEntry(path, exec == null));
+                paths.add(new LocationEntry(path, exec == null, predicate));
     }
 
     public LocationRequest makeRequest(String oldLocation, Consumer<File> foundFile) {
@@ -58,10 +63,12 @@ public class LocationTarget {
     private static class LocationEntry {
         private final String[] fileParts;
         private final boolean asContainer;
+        private Predicate<File> predicate;
 
-        private LocationEntry(String path, boolean asContainer) {
+        private LocationEntry(String path, boolean asContainer, Predicate<File> predicate) {
             fileParts = path.split("/");
             this.asContainer = asContainer;
+            this.predicate = predicate;
         }
 
         private File matches(File file) {
@@ -72,7 +79,8 @@ public class LocationTarget {
                 orig = file;
                 file = file.getParentFile();
             }
-            return asContainer ? file : orig;
+            File result = asContainer ? file : orig;
+            return predicate != null && !predicate.test(result) ? null : result;
         }
     }
 }
