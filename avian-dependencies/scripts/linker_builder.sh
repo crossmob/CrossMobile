fa90ddf2698ab81ed23134e97eb6a0611ce021cc
Farshid <monhaseri.f@gmail.com>
2021-04-01 17:19:50 +0430

    scripts: Provided host gcc objects & shared libs
    Signed-off-by: Farshid <monhaseri.f@gmail.com>

--- avian-dependencies/scripts/linker_builder.sh
+++ avian-dependencies/scripts/linker_builder.sh
@@ -36,98 +36,151 @@ NUM_PROC=$(nproc)
 #----------------linker--------------------
 SRC_ROOT="$(dirname $(realpath "$0"))/.."
 TOOLS_DIR="$SRC_ROOT/target/common/bin"
-LD_LINKER_SRC="${SRC_ROOT}/aroma-ld-linker"
-LD_LINKER_ZIP="${LD_LINKER_SRC}.tar.bz2"
-CYGWIN_LINKER_SRC="${SRC_ROOT}/aroma-cygwin-linker"
+UTIL_GNU_SRC="${SRC_ROOT}/aroma-ld-linker"
+UTIL_GNU_ZIP="${UTIL_GNU_SRC}.tar.bz2"
+#UTIL_WIN_SRC="${SRC_ROOT}/aroma-cygwin-linker"
+UTIL_WIN_SRC="${SRC_ROOT}/mingw-w64"
+UTIL_WIN_BRANCH="master"
+
 AROMA_LINKER_HOST_TRIPLE=""
 AROMA_LINKER_TARGET_TRIPLE=""
 CMD=""
+GCC_VER="8"
 
 _source_cygwin_check() {
-    if [ ! -d $CYGWIN_LINKER_SRC ]; then
+    if [ ! -d $UTIL_WIN_SRC ]; then
         cd $SRC_ROOT
-        git clone --depth 1 https://github.com/cygwin/cygwin.git -b cygwin-3_1_7-release
-        mv cygwin $CYGWIN_LINKER_SRC
+        git clone --depth 1 https://github.com/cygwin/cygwin.git -b $UTIL_WIN_BRANCH
+        mv cygwin $UTIL_WIN_SRC
     fi
+    cd $UTIL_WIN_SRC
+    git checkout $UTIL_WIN_BRANCH
+    cd $SRC_ROOT
 }
 
 _source_ld_check() {
-    if [ ! -d $LD_LINKER_SRC ]; then
-        if [ ! -f $LD_LINKER_ZIP ]; then
-            wget -O $LD_LINKER_ZIP https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.bz2
+    if [ ! -d $UTIL_GNU_SRC ]; then
+        if [ ! -f $UTIL_GNU_ZIP ]; then
+            wget -O $UTIL_GNU_ZIP https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.bz2
         fi
-        tar -xvf $LD_LINKER_ZIP --one-top-level=$LD_LINKER_SRC --strip-components 1
+        tar -xvf $UTIL_GNU_ZIP --one-top-level=$UTIL_GNU_SRC --strip-components 1
     fi
 }
 
 _clean() {
-    local LINKER_BUILD_DIR="$LD_LINKER_SRC/build/${1}__${2}"
-    local TOOLS_DIR_INSTALL="$TOOLS_DIR/${1}__${2}"
-    __msg_warn "Deleting $LINKER_BUILD_DIR"
-    rm -rf $LINKER_BUILD_DIR
-    __msg_warn "Deleting $TOOLS_DIR_INSTALL"
-    rm -rf $TOOLS_DIR_INSTALL
+    local BUILD_DIR="$UTIL_GNU_SRC/build/${1}__${2}"
+    local TOOLS_INSTALL_DIR="$TOOLS_DIR/${1}__${2}"
+
+    __msg_warn "Deleting $BUILD_DIR"
+    rm -rf $BUILD_DIR
+    __msg_warn "Deleting $TOOLS_INSTALL_DIR"
+    rm -rf $TOOLS_INSTALL_DIR
 }
 
 _build() {
 
     local PREFIX_BIN="aroma-"
     local IS_GNU=true
+    local HOST_TRIPLE=$1
+    local TARGET_TRIPLE=$2
+    local HOST_TRIPLE_LIB=$1
+
     if [[ "$1" == *"linux"* ]]; then
         PREFIX_BIN="${PREFIX_BIN}gnu-"
-        LINKER_SRC=$LD_LINKER_SRC
+        LINKER_SRC=$UTIL_GNU_SRC
     elif [[ "$1" == *"w64"* || "$1" == *"w32"* ]]; then
         SUFFIX_BIN=".exe"
         PREFIX_BIN="${PREFIX_BIN}cygwin-"
-        LINKER_SRC=$CYGWIN_LINKER_SRC
+        LINKER_SRC=$UTIL_WIN_SRC
         IS_GNU=false
+	    HOST_TRIPLE_LIB=i386-linux-gnu
     else
-        __msg_error "host system is not supported: %1"
+        __msg_error "host system is not supported: $1"
     fi
 
-    local PREFIX_DIR="/user"
-    local LINKER_BUILD_DIR="$LINKER_SRC/build/${1}__${2}"
-    local LINKER_DIR_TEMP="$LINKER_BUILD_DIR/install"
-    local LINKER_BIN_TEMP="$LINKER_DIR_TEMP${PREFIX_DIR}/bin/${PREFIX_BIN}ld${SUFFIX_BIN}"
-    local TOOLS_DIR_INSTALL="$TOOLS_DIR/${1}__${2}"
-    local LINKER_BIN_INSTALL="$TOOLS_DIR_INSTALL/${PREFIX_BIN}ld${SUFFIX_BIN}"
-    local AR_BIN_TEMP="$LINKER_DIR_TEMP/${PREFIX_DIR}/bin/${PREFIX_BIN}ar${SUFFIX_BIN}"
-    local AR_BIN_INSTALL="$TOOLS_DIR_INSTALL/${PREFIX_BIN}ar${SUFFIX_BIN}"
-    if [ ! -f LINKER_BIN_INSTALL ]; then
-        if [ ! -f $LINKER_BIN_TEMP ]; then
+    local LIB_ROOT="/lib"
+    local LIB_ROOT_TARGET="/lib/$HOST_TRIPLE_LIB"
+    local LIB_USR="/usr/lib"
+    local LIB_USR_TARGET="/usr/lib/$HOST_TRIPLE_LIB"
+    local LIB_GCC_TARGET="/usr/lib/gcc/$HOST_TRIPLE_LIB/$GCC_VER"
+    local LIB_GCC_CROSS="/usr/lib/gcc-cross/$HOST_TRIPLE_LIB/$GCC_VER"
+    local BUILD_DIR="$LINKER_SRC/build/${1}__${2}"
+    local TEMP_INSTALL_PREFIX="/user"
+    local TEMP_INSTALL_DIR="$BUILD_DIR/install"
+    local TEMP_LD_BIN="${TEMP_INSTALL_DIR}${TEMP_INSTALL_PREFIX}/bin/${PREFIX_BIN}ld${SUFFIX_BIN}"
+    local TEMP_AR_BIN="${TEMP_INSTALL_DIR}${TEMP_INSTALL_PREFIX}/bin/${PREFIX_BIN}ar${SUFFIX_BIN}"
+    local TOOLS_INSTALL_DIR="$TOOLS_DIR/${1}__${2}"
+    local TOOLS_INSTALL_LIB_DIR=$TOOLS_INSTALL_DIR/lib
+    local LD_INSTALL_BIN="$TOOLS_INSTALL_DIR/${PREFIX_BIN}ld${SUFFIX_BIN}"
+    local AR_BIN_INSTALL="$TOOLS_INSTALL_DIR/${PREFIX_BIN}ar${SUFFIX_BIN}"
+
+    if [ ! -f $LD_INSTALL_BIN ]; then
+        if [ ! -f $TEMP_LD_BIN ]; then
             if [ "$IS_GNU" = true ]; then
+                __msg_info "Building GNU Binutils ..."
                 _source_ld_check
-                [ -d $LINKER_BUILD_DIR ] && \
-                    rm -rf $LINKER_BUILD_DIR
-                mkdir -p $LINKER_BUILD_DIR
-                cd $LINKER_BUILD_DIR
+                [ -d $BUILD_DIR ] && \
+                    rm -rf $BUILD_DIR
+                mkdir -p $BUILD_DIR
+                cd $BUILD_DIR
                 $LINKER_SRC/configure \
                 --host=$AROMA_LINKER_HOST_TRIPLE \
                 --target=$AROMA_LINKER_TARGET_TRIPLE \
                 --program-prefix=$PREFIX_BIN \
-                --prefix=$PREFIX_DIR \
+                --prefix=$TEMP_INSTALL_PREFIX \
                 --enable-ld=yes \
                 --with-static-standard-libraries
+
+                mkdir -p $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_USR_TARGET/Scrt1.o $TOOLS_INSTALL_LIB_DIR 
+                cp $LIB_USR_TARGET/crti.o  $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_USR_TARGET/crtn.o  $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_GCC_TARGET/crtbeginS.o  $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_GCC_TARGET/crtendS.o    $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_GCC_TARGET/libgcc.so    $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_GCC_TARGET/libgcc_s.so  $TOOLS_INSTALL_LIB_DIR
+                cp $LIB_GCC_TARGET/libstdc++.so $TOOLS_INSTALL_LIB_DIR
             else
+                __msg_info "building Windows Bintuils ..."
                 _source_cygwin_check
-                [ -d $LINKER_BUILD_DIR ] && \
-                    rm -rf $LINKER_BUILD_DIR
-                mkdir -p $LINKER_BUILD_DIR
-                cd $LINKER_BUILD_DIR
+                [ -d $BUILD_DIR ] && \
+                    rm -rf $BUILD_DIR
+                mkdir -p $BUILD_DIR
+                cd $BUILD_DIR
+                
+                CROSS_PREFIX="x86_64-w64-mingw32"
+                export CROSS_COMPILE="${CROSS_PREFIX}-"
+                export CC="$CROSS_PREFIX-gcc"
+                export NM="$CROSS_PREFIX-nm"
+                export LD="$CROSS_PREFIX-ld"
+                export CXX="$CROSS_PREFIX-g++"
+                export RANLIB="$CROSS_PREFIX-ranlib"
+                export AR="$CROSS_PREFIX-ar"
+                #DIR_CROSS_INC="/usr/include/i386-linux-gnu"
+		        DIR_CROSS_INC="/usr/include/arm-linux-gnueabihf"
+                DIR_CROSS_INC_STD="$DIR_CROSS_INC/c++/8"
+		        #DIR_CROSS_INC_STD_0="/usr/arm-linux-gnueabihf/include"
+		        #DIR_CROSS_INC_STD_1="/usr/i686-linux-gnu/include"
+                DIR_CROSS_LIBS="/usr/lib/i386-linux-gnu"
+                export LD_LIBRARY_PATH="$DIR_CROSS_LIBS:/usr/lib/$2"
+                export LDFLAGS="$LDFLAGS -L$DIR_CROSS_LIBS -L/usr/lib$2 -pie"
+                export PKG_CONFIG_PATH="$PKG_CONFIG_PATH $DIR_CROSS_LIBS/pkgconfig"
+                export CFLAGS="$CFLAGS -I$DIR_CROSS_INC -I$DIR_CROSS_INC_STD -I$DIR_CROSS_INC_STD_0 -I$DIR_CROSS_INC_STD_1"
+
                 $LINKER_SRC/configure \
                 --host=$AROMA_LINKER_HOST_TRIPLE \
                 --target=$AROMA_LINKER_TARGET_TRIPLE \
                 --program-prefix=$PREFIX_BIN \
-                --prefix=$PREFIX_DIR \
+                --prefix=$TEMP_INSTALL_PREFIX \
                 --enable-ld=yes
             fi
             make -j $NUM_PROC
-            mkdir -p $LINKER_DIR_TEMP
-            DESTDIR=$LINKER_DIR_TEMP make install
+            mkdir -p $TEMP_INSTALL_DIR
+            DESTDIR=$TEMP_INSTALL_DIR make install
         fi
-        mkdir -p $TOOLS_DIR_INSTALL
-        cp $LINKER_BIN_TEMP $TOOLS_DIR_INSTALL
-        cp $AR_BIN_TEMP $TOOLS_DIR_INSTALL
+        mkdir -p $TOOLS_INSTALL_DIR
+        cp $TEMP_LD_BIN $TOOLS_INSTALL_DIR
+        cp $TEMP_AR_BIN $TOOLS_INSTALL_DIR
     fi
 }
 
