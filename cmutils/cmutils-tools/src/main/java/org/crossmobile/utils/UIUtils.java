246248ec9c31b924d0a95b56b61b129ff3c5759e
Panayotis Katsaloulis <panayotis@panayotis.com>
2022-09-03 01:39:48 +0300

    📐 Support HiDPI desktop launching by default

--- cmutils/cmutils-tools/src/main/java/org/crossmobile/utils/UIUtils.java
+++ cmutils/cmutils-tools/src/main/java/org/crossmobile/utils/UIUtils.java
@@ -27,4 +27,20 @@ public class UIUtils {
         }
     }
 
+    public static double getScaleBasedOnDPI(int DPI) {
+        double scale = DPI / 96d;
+        if (scale < 1.1) return 1;
+        else if (scale < 1.35) return 1.25;
+        else if (scale < 1.6) return 1.5;
+        else if (scale < 1.8) return 1.75;
+        else if (scale < 2.1) return 2;
+        else if (scale < 2.3) return 2.25;
+        else if (scale < 2.6) return 2.5;
+        else if (scale < 3.1) return 3;
+        else if (scale < 3.6) return 3.5;
+        else if (scale < 4.2) return 4;
+        else return 5;
+    }
+
+
 }
