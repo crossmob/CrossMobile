a3ebc3847addca48baba83809543f09e164599d0
Panayotis Katsaloulis <panayotis@panayotis.com>
2022-11-06 20:53:32 +0200

    🛠️ update bootstrap to use crossmobile repository instead of missing android-rebuilds.beuc.net

--- resources/bin/bootstrap.sh
+++ resources/bin/bootstrap.sh
@@ -10,9 +10,9 @@ SDK_FULL=$SDK.0.0_r36
 set -e
 OUT=/tmp/crossmobile.temp.android
 rm -rf $OUT && mkdir $OUT
-curl https://android-rebuilds.beuc.net/dl/repository/sdk-repo-linux-platforms-eng.$SDK_FULL.zip -o $OUT/sdk.zip
-unzip -o -j $OUT/sdk.zip -d $OUT
+curl -L https://github.com/crossmob/cmbuild/releases/download/gradle-dependencies/android-12.0.0_r13.jar -o $OUT/android.jar
 mvn install:install-file -Dfile=$OUT/android.jar -DgroupId=org.crossmobile.ca.com.google.android -DartifactId=android -Dversion=$VERSION -Dpackaging=jar
+rm $OUT/android.jar
 
 cd $(dirname $0) && cd ../../cmioslayer
 if [ $DEPLOY ] ; then 
